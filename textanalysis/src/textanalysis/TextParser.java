568a176628dfb7509e1377d470fc598096e2feab
Ting Shen <stgogogo@live.cn>
2018-02-11 00:32:48 -0600

    edit test

--- textanalysis/src/textanalysis/TextParser.java
+++ textanalysis/src/textanalysis/TextParser.java
@@ -2,10 +2,10 @@ package textanalysis;;
 
 public class TextParser {
 	public static void main(String[] args){
-		Parser parser = new Parser("What's the weather in Auburn on February 21st, 2018 at 5:23 pm");
-		
-		for(int i=0; i<parser.parts.length; i++){
-			System.out.println(parser.parts[i]);
-		}
+		Parser parser = new Parser("What's the weather in Auburn at 5:23 pm yesterday?");
+		System.out.println(parser.getlat());
+		System.out.println(parser.getlon());
+		System.out.println(parser.getdate());
+		System.out.println(parser.gettime());
 	}
 }
